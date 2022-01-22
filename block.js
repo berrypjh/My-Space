de44feee48a821c40fd0d01c5542c62af93cb4f3
Park Jun Hyuk <junhyeg08@gmail.com>
2022-01-22 23:16:32 +0900

    Feat: BlockChain 만들기

--- block.js
+++ block.js
@@ -6,8 +6,9 @@ class Block {
 };
 
 class BlockHeader {
-  constructor(version, previousHash, timestamp, merkleRoot, bit, nonce) {
+  constructor(version, index, previousHash, timestamp, merkleRoot, bit, nonce) {
     this.version = version;
+    this.index = index;
     this.previousHash = previousHash;
     this.timestamp = timestamp;
     this.merkleRoot = merkleRoot;
@@ -15,3 +16,81 @@ class BlockHeader {
     this.nonce = nonce;
   };
 };
+
+const fs = require('fs');
+const merkle = require('merkle');
+const cryptojs = require('crypto-js');
+
+function getVersion() {
+  const package = fs.readFileSync("package.json");
+  return JSON.parse(package).version;
+}
+
+function createGenesisBlock() {
+  const version = getVersion();
+  const index = 0;
+  const previousHash = '0'.repeat(64);
+  const timestamp = parseInt(Date.now() / 1000);
+  const body = ['hello block'];
+  const tree = merkle('sha256').sync(body);
+  const merkleRoot = tree.root() || '0'.repeat(64);
+  const bit = 0;
+  const nonce = 0;
+
+  const header = new BlockHeader(version, index, previousHash, timestamp, merkleRoot, bit, nonce);
+
+  return new Block(header, body);
+}
+
+let Blocks = [createGenesisBlock()];
+
+function getBlocks() {
+  return Blocks;
+}
+
+function getLastBlock() {
+  return Blocks[Blocks.length - 1];
+}
+
+function createHash(data) {
+  const { version, previousHash, timestamp, merkleRoot, bit, nonce } = data.header;
+  const blockString = version + previousHash + timestamp + merkleRoot + bit + nonce;
+  const hash = cryptojs.SHA256(blockString).toString();
+
+  return hash;
+}
+
+// const block = createGenesisBlock();
+// const testHash = createHash(block);
+// console.log(testHash);
+
+function nextBlock(bodyData) {
+  const prevBlock = getLastBlock();
+  const version = getVersion();
+  const index = prevBlock.header.index + 1;
+  const previousHash = createHash(prevBlock);
+  const timestamp = parseInt(Date.now() / 1000)
+  const tree = merkle('sha256').sync(bodyData);
+  const merkleRoot = tree.root() || '0'.repeat(64);
+  const bit = 0;
+  const nonce = 0;
+
+  const header = new BlockHeader(version, index, previousHash, timestamp, merkleRoot, bit, nonce);
+  return new Block(header, bodyData);
+}
+
+// const block1 = nextBlock(['transaction1']);
+// console.log(block1);
+
+function addBlock(bodyData) {
+  const newBlock = nextBlock(bodyData);
+  Blocks.push(newBlock);
+}
+
+addBlock(['transaction1']);
+addBlock(['transaction2']);
+addBlock(['transaction3']);
+addBlock(['transaction4']);
+addBlock(['transaction5']);
+
+console.log(Blocks);
