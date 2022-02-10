d1ae1c835c0be5e6e50c23240d11cfba78c442d8
Junhyuk Park <junhyeg08@gmail.com>
2022-02-10 17:43:36 +0900

    Feat: 블록체인 POW 구현 ① (마지막 블록 받기)

--- p2pServer.js
+++ p2pServer.js
@@ -1,5 +1,6 @@
 const WebSocket = require("ws");
 const { WebSocketServer } = require("ws");
+const { getLastBlock } = require("./block");
 
 const initP2PServer = (ws_port) => {
   const server = new WebSocketServer({ port: ws_port });
@@ -14,9 +15,25 @@ const initP2PServer = (ws_port) => {
 
 let sockets = [];
 
+const queryLatestMsg = () => {
+  return {
+    type: MessageType.QUERY_LATEST,
+    data: null,
+  };
+};
+
+const responseLatestMsg = () => {
+  return {
+    type: MessageType.RESPONSE_BLOCKCHAIN,
+    data: JSON.stringify([getLastBlock()]),
+  };
+};
+
 const initConnection = (ws) => {
   sockets.push(ws);
+  initMessageHandler(ws);
   initErrorHandler(ws);
+  write(ws, queryLatestMsg());
 };
 
 const getSockets = () => {
@@ -48,6 +65,42 @@ const initErrorHandler = (ws) => {
 const closeConnection = (ws) => {
   console.log(`Connection close ${ws.url}`);
   sockets.splice(sockets.indexOf(ws), 1);
+};
+
+const write = (ws, message) => {
+  ws.send(JSON.stringify(message));
+};
+
+const MessageType = {
+  QUERY_LATEST: 0,
+  QUERY_ALL: 1,
+  RESPONSE_BLOCKCHAIN: 2,
+};
+
+const initMessageHandler = (ws) => {
+  ws.on("message", (data) => {
+    const message = JSON.parse(data);
+
+    if (message === null) {
+      return;
+    };
+
+    switch (message.type) {
+      case MessageType.QUERY_LATEST:
+        write(ws, responseLatestMsg());
+        break;
+      case MessageType.QUERY_ALL:
+        console.log("QUERY_ALL");
+        console.log(message);
+        // 추가 예정
+        break;
+      case MessageType.RESPONSE_BLOCKCHAIN:
+        console.log("RESPONSE_BLOCKCHAIN");
+        console.log(message);
+        // 추가 예정
+        break;
+    };
+  });
 };
 
 module.exports = {
